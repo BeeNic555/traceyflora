2f38fad120b6549cf6bb0ab7797d991f4c253b92
Hank <fenghaocui@Hanks-NokiaBook.local>
2025-05-09 20:42:34 -0400

    update

--- main/views.py
+++ main/views.py
@@ -7,8 +7,14 @@ def home(request):
     image_list = [
         'main/images/1.png',
         'main/images/2.png',
-        'main/images/3.png',
         'main/images/4.png',
+        'main/images/15.png',
+        'main/images/14.png',
+        'main/images/16.png',
+        'main/images/17.png',
+        'main/images/19.png',
+        'main/images/21.png',
+        'main/images/24.png',
         # 添加更多图片路径
     ]
     return render(request, 'main/home.html', {'image_list': image_list})
@@ -29,10 +35,26 @@ def showcase(request):
         'main/images/2.png',
         'main/images/3.png',
         'main/images/4.png',
-        'main/images/1.png',
-        'main/images/2.png',
-        'main/images/3.png',
-        'main/images/4.png',
+        'main/images/5.png',
+        'main/images/6.png',
+        'main/images/7.png',
+        'main/images/8.png',
+        'main/images/9.png',
+        'main/images/10.png',
+        'main/images/11.png',
+        'main/images/12.png',
+        'main/images/13.png',
+        'main/images/14.png',
+        'main/images/15.png',
+        'main/images/16.png',
+        'main/images/17.png',
+        'main/images/18.png',
+        'main/images/19.png',
+        'main/images/20.png',
+        'main/images/21.png',
+        'main/images/22.png',
+        'main/images/23.png',
+        'main/images/24.png'
         # 添加更多图片路径
     ]
     return render(request, 'main/showcase.html', {'image_list': image_list})
